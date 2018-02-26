6ce20d7b649def57b4aa4185429dfc1b0ba36d3f
Victoria Rubtsova <36713256+visha-v@users.noreply.github.com>
2018-02-26 04:20:42 +0300

    Add files via upload

---
+++ Important Birthday.py
@@ -0,0 +1,14 @@
+age = eval(input("Enter age: "))
+
+if (age >= 1) and (age <= 18):
+    print ("Important Birthday")
+
+elif (age ==21) or (age == 50):
+    print("Important Birthday")
+
+elif not (age < 65):
+    print("Important Birthday")
+
+else:
+    print("Sorry Not Important")
+
