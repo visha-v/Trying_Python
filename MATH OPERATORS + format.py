219bcad459cbada3468def39c219a8b03fc64ee1
Victoria Rubtsova <36713256+visha-v@users.noreply.github.com>
2018-02-26 04:21:29 +0300

    Add files via upload

---
+++ MATH OPERATORS + format.py
@@ -0,0 +1,21 @@
+num1, num2 = input('Enter 2 numbers: ').split()
+
+num1 = int(num1)
+num2 = int(num2)
+
+sum = num1 + num2
+
+difference = num1 - num2
+
+product = num1 * num2
+
+quotient = num1 / num2
+
+remainder = num1 % num2
+
+# Print the results
+print("{} + {} = {}".format(num1,num2,sum))
+print("{} - {} = {}".format(num1,num2,difference))
+print("{} * {} = {}".format(num1,num2,product))
+print("{} / {} = {}".format(num1,num2,quotient))
+print("{} % {} = {}".format(num1,num2,remainder))
