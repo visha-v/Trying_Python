8c288d96ddfd543ea135333d59ff2b2974371f0a
Maxim Veysgeym <qewwork@rambler.ru>
2018-02-27 15:59:47 +0300

    Refactored viriable names

--- geek.py
+++ geek.py
@@ -2,10 +2,10 @@ import os
 import sys
 import psutil
 
-do = ' '
+process_is_active = true
 
-while do != 0:
-    do = input('''
+while process_is_active:
+    command = input('''
     Введите номер нужной функции:
     [1] - Содержание текущей директории
     [2] - Номера выполняемых в системе процессов
@@ -15,22 +15,24 @@ while do != 0:
     [6] - Имя пользователя
     
     ''')
-    if do == ('1'):
+    if command == ('1'):
         print(os.listdir())   # Список файлов в текущей директории
-    elif do == ('2'):
+    elif command == ('2'):
         print(psutil.pids())  # Список номеров запущенных в системе процессов PID
-    elif do == ('3'):
+    elif command == ('3'):
         print(psutil.cpu_count()) # Кол-во ядер проца
-    elif do == ('4'):
+    elif command == ('4'):
         print(os.getcwd()) # Текущая директория
-    elif do == ('5'):
+    elif command == ('5'):
         print(sys.getfilesystemencoding()) # Кодировка файловой системы
-    elif do == ('6'):
+    elif command == ('6'):
         print(os.getlogin()) # Имя текущего пользователя системы
+    elseif command == ('0'):
+        process_is_active = false
+        print('Пока!')
     else:
         print('Некорректное значение!')
-if do == 0:
-    print('Пока!')
+    
 
 
 
