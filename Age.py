77d3685a4cbfb79a1cbb841644d4664ef9a1afb6
Maxim Veysgeym <qewwork@rambler.ru>
2018-03-01 14:08:32 +0300

    Made so app grew some balls

--- Age.py
+++ Age.py
@@ -10,5 +10,7 @@ elif (age > 5) and (age <= 17):
     grade = age - 5
     print("Go to grade {}!".format(grade))
 
-else:
+elsif age < 21:
     print ("Go to college!")
+else:
+    print("GO TO WORK")
