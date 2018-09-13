988e15fe0d482100e2340545be5d2d15600f582f
visha <visha.venik@gmail.com>
2018-09-13 20:27:21 +0300

    misprint fixed

--- Age.py
+++ Age.py
@@ -10,7 +10,7 @@ elif (age > 5) and (age <= 17):
     grade = age - 5
     print("Go to grade {}!".format(grade))
 
-elsif age < 21:
+elif age < 21:
     print ("Go to college!")
 else:
     print("GO TO WORK")
