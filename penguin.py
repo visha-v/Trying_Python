7678ea400661ea0dea0eb6d392edefa8c8395f53
Victoria <visha.venik@gmail.com>
2018-10-16 02:35:33 +0300

    added New prog

---
+++ penguin.py
@@ -0,0 +1,16 @@
+
+amount = int(input())
+
+
+p1 = '     _~_        '
+p2 = '    (o o)       '
+p3 = '   /  V  \\      '
+p4 = '  /(  _  )\\     '
+p5 = '    ^^ ^^       '
+
+
+print(p1*amount)
+print(p2*amount)
+print(p3*amount)
+print(p4*amount)
+print(p5*amount)
